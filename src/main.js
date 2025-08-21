77ef857d4193ca58a07fc8a423b61a44b06cca84
Max Huber <max@maxhuber.se>
2025-08-21 16:07:00 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
-const projects = document.getElementsByName("project");
-projects.forEach((project) => {
-  project.addEventListener("click", (e) => {
-    if (e.target === project) hideProject(project.id);
+// Set year dynamically (at bottom, by copyright notice)
+document.getElementById("year").textContent = new Date().getFullYear();
+
+const sections = {
+  about: "section-about",
+  projects: "section-projects",
+  contact: "section-contact",
+};
+
+let projects = [];
+
+function fetchPages() {
+  const sectionKeys = Object.keys(sections);
+  for (const section of sectionKeys) {
+    fetch(`sections/${section}.html`)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById(`section-${section}`).innerHTML = html;
+
+      if(section == sectionKeys[sectionKeys.length - 1]) {
+        loadProjects();
+      }
+    });
+  }
+}
+
+function loadProjects() {
+  projects = document.getElementsByName("project");
+  projects.forEach((project) => {
+    project.addEventListener("click", (e) => {
+      if (e.target === project) hideProject(project.id);
+    });
   });
-});
+}
 
 function showProject(cardId) {
   projects.forEach((project) => {
@@ -25,19 +53,4 @@ function hideAllProjects() {
   projects.forEach((project) => project.classList.add("hidden"));
 }
 
-// Set year dynamically (at bottom, by copyright notice)
-document.getElementById("year").textContent = new Date().getFullYear();
-
-const sections = {
-  about: "section-about",
-  projects: "section-projects",
-  contact: "section-contact",
-};
-
-for (const section of Object.keys(sections)) {
-  fetch(`sections/${section}.html`)
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById(`section-${section}`).innerHTML = html;
-    });
-}
+fetchPages();
